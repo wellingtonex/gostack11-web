ed90500bd7ba82a2e12ecfcf94f57dd8ec1bb085
wellingtonx <wellingtonex@gmail.com>
2020-05-05 01:10:31 -0300

    componentizando input e button

--- src/pages/Signin/index.tsx
+++ src/pages/Signin/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 
 import { Container, Content, Background } from './style';
 import logoImg from '../../assets/logo.svg';
+
+import Button from '../../components/Button';
+import Input from '../../components/Input';
 
 const Signin: React.FC = () => (
   <Container>
@@ -11,10 +14,15 @@ const Signin: React.FC = () => (
 
       <form>
         <h1>Faça seu logon</h1>
-        <input placeholder="E-mail" />
-        <input type="password" placeholder="Senha" />
+        <Input name="email" icon={FiMail} placeholder="E-mail" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Senha"
+        />
 
-        <button type="submit">Entrar</button>
+        <Button type="submit">Entrar</Button>
         <a href="/forgot">Esqueci minha senha</a>
       </form>
       <a href="">
